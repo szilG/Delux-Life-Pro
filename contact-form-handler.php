9ad6b5e77391a1261a0304a0ee8c0bb93bb43708
szilG <gombiszilvi@gmail.com>
2021-02-07 10:12:41 +0000

    add jquery lightbox add gallery images

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,21 +1,25 @@
 <?php
 
-$name = $_POST["name"];
-$email = $_POST["email"];
-$phone = $_POST["phone"];
-$message1 = $_POST["message"];
-$message2 = $_POST["message2"];
+if(filter_has_var(INPUT_POST, "submit")) {
+    echo "subbmited";
+}
 
-$to = "gombiszilvi@gmail.com";
-$body = "";
+// $name = $_POST["name"];
+// $email = $_POST["email"];
+// $phone = $_POST["phone"];
+// $message1 = $_POST["message"];
+// $message2 = $_POST["message2"];
 
-$body .= "Név: ".$name. "\r\n"; 
-$body .= "Email: ".$email. "\r\n";
-$body .= "Telefon: " .$phone. "\r\n";
-$body .= "Kitöl kapta ezt az email címet: " .$message1. "\r\n";
-$body .= "Üzenet = " .$message2;
+// $to = "gombiszilvi@gmail.com";
+// $body = "";
 
-mail($to,$message1,$body);
+// $body .= "Név: ".$name. "\r\n"; 
+// $body .= "Email: ".$email. "\r\n";
+// $body .= "Telefon: " .$phone. "\r\n";
+// $body .= "Kitöl kapta ezt az email címet: " .$message1. "\r\n";
+// $body .= "Üzenet = " .$message2;
+
+// mail($to,$message1,$body);
 
 
 
